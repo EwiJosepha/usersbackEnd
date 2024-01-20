8f53f11ae88edd55c40c400a84ed41f4a7382165
ewi josepha <ewijosepha@gmail.com>
2024-01-20 13:22:53 +0100

    adding back and front end

---
+++ clients/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [backenddata, setBackenddata] = useState([{}])
+  useEffect(()=>{
+fetch("/api").then((res)=> res.json()).then((data)=> {
+  setBackenddata(data)
+  console.log(data);
+})
+  },[])
+
+  return (
+    <>
+    {(typeof backenddata.users === 'undefined')?(  <p>LOADING...</p>): backenddata.users.map((user, i)=>( 
+      <p key={i}>{user}</p>)
+    )}
+  
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+// const PORT = process.env.PORT || 5000
+
+app.get("/api", ((req, res)=>{
+  res.json({"users": ["userOne", 'userTwo', 'userThree']})
+}))
+
+app.listen(5000, ()=>{
+  console.log("server started on port 5000");
+})
