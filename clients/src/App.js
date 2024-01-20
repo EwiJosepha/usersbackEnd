1138d689bdff187a68249fb751bba349519f1598
ewi josepha <ewijosepha@gmail.com>
2024-01-20 13:39:56 +0100

    adding back and front end

--- clients/src/App.js
+++ clients/src/App.js
@@ -3,19 +3,19 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [backenddata, setBackenddata] = useState([{}])
-  useEffect(()=>{
-fetch("/api").then((res)=> res.json()).then((data)=> {
-  setBackenddata(data)
-  console.log(data);
-})
-  },[])
+  useEffect(() => {
+    fetch("/api").then((res) => res.json()).then((data) => {
+      setBackenddata(data)
+      console.log(data);
+    })
+  }, [])
 
   return (
     <>
-    {(typeof backenddata.users === 'undefined')?(  <p>LOADING...</p>): backenddata.users.map((user, i)=>( 
-      <p key={i}>{user}</p>)
-    )}
-  
+      {(typeof backenddata.users === 'undefined') ? (<p>LOADING...</p>) : backenddata.users.map((user, i) => (
+        <p key={i}>{user}</p>)
+      )}
+
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.get("/api", ((req, res)=>{
   res.json({"users": ["userOne", 'userTwo', 'userThree']})
 }))
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
   console.log("server started on port 5000");
 })
